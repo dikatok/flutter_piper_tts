83cfc22ccdb6c186ee74e9e0c26fb11a4d6756f6
Andika Tanuwijaya <holy.arc.64633@gmail.com>
2026-05-08 19:29:28 +0700

    skip building native asset for intel mac, ort crate does not support it

--- packages/dart_piper_tts/hook/build.dart
+++ packages/dart_piper_tts/hook/build.dart
@@ -1,8 +1,17 @@
+import 'dart:io';
+
+import 'package:code_assets/code_assets.dart';
 import 'package:hooks/hooks.dart';
 import 'package:native_toolchain_rust/native_toolchain_rust.dart';
 
 void main(List<String> args) async {
   await build(args, (input, output) async {
+    if (input.config.code.targetArchitecture == Architecture.x64 &&
+        input.config.code.targetOS == OS.macOS) {
+      stdout.write('Skipping intel mac, ort does not support it');
+      return;
+    }
+
     await RustBuilder(
       assetName: 'src/ffi.g.dart',
     ).run(input: input, output: output);
