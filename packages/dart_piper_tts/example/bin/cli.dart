3aed0f27ead6bad1ecbfc7b5cbe435152c275a7a
Andika Tanuwijaya <holy.arc.64633@gmail.com>
2026-05-14 23:15:18 +0700

    fix cli example

--- packages/dart_piper_tts/example/bin/cli.dart
+++ packages/dart_piper_tts/example/bin/cli.dart
@@ -3,7 +3,10 @@ import 'dart:io';
 import 'package:dart_piper_tts/dart_piper_tts.dart';
 
 void main(List<String> arguments) {
-  PiperTTS.init((dataDir: "./"));
+  PiperTTS.init((
+    phonemizerModelPath:
+        "../../flutter_piper_tts/assets/phonemizer/g2p-mbyt5-12l-ipa-childes-espeak-onnx-quantized.onnx",
+  ));
   final tts = PiperTTS.create((
     configPath: "./en_US-hfc_female-medium.onnx.json",
     modelPath: "./en_US-hfc_female-medium.onnx",
